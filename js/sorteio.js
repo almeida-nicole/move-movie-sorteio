a89ad891fd0a05e3a410343440ea441ddcc4bfad
Nicole Almeida <almeida-nicole@live.com>
2018-12-02 14:19:59 -0200

    configura sorteio dos filmes
    Foi adicionado html, javascript e arquivos sass

---
+++ js/sorteio.js
@@ -0,0 +1,41 @@
+const button = document.querySelector('.filmes__sortear');
+const form = document.querySelector('.filmes__form');
+const tituloEscolhido = document.querySelector('.filmes__titulo-escolhido');
+const filmesBox = document.querySelector('.filmes__sorteado');
+
+form.addEventListener('submit', function(e){
+e.preventDefault();
+// pegar os filmes
+const filmes = getFilmes();
+// sortear os filmes
+const filmeSorteado = sorteiaFilme(filmes);
+// sumir com os inputs e mostrar o filme escolhido
+mostrarFilmeEscolhido(filmeSorteado);
+
+})
+
+
+function sorteiaNumero(numero) {
+  return Math.ceil(Math.random() * numero);
+}
+
+function mostrarFilmeEscolhido(filme) {
+  tituloEscolhido.textContent = filme;
+  form.classList.add('is-hidden');
+  filmesBox.classList.remove('is-hidden');
+}
+
+function getFilmes() {
+  let filmes = [];
+  document.querySelectorAll('.filmes__input').forEach(filme => {
+    if(filme.value.length > 0){
+      filmes.push(filme)
+    }
+  });
+  console.log(filmes)
+  return filmes;
+}
+
+function sorteiaFilme(filmes) {
+  return filmes[sorteiaNumero(filmes.length) - 1].value;
+}
